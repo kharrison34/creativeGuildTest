01fd3bad235ee018311f486976751867870b3e5f
Kyle Harrison <kyle.harrison344@gmail.com>
2020-10-15 19:32:39 -0400

    adding styling

--- client/src/components/FetchData.js
+++ client/src/components/FetchData.js
@@ -53,29 +53,32 @@ export default class FetchData extends React.Component {
                <div>
                     <Row>
                         <Col> 
-                        <Media className="my-3 p-3 mt-5" >
-                         <img className="Rounded proImg" src={profilePic} alt="profile picture" />
-                            <Media heading className="p-3 medHead">
-                                <h1>
-                                 {this.state.name}
-                                </h1>
-                                <p>
-                                 {this.state.bio}
+                            <Media className="my-3 p-3 mt-5 media-div" >
+                            <img className="Rounded proImg" src={profilePic} alt="profile picture" />
+                                <Media heading className="p-3 medHead">
+                                    <h1>
+                                    {this.state.name}
+                                    </h1>
+                                
+                                    <p className="bio">Bio</p>
+                             
+                                    <p>
+                                    {this.state.bio}
 
-                                </p>
+                                    </p>
+                                </Media>
+                                <Media body className="">
+                                    <div className="medBody">
+                                        <h3>Phone</h3>
+                                        <p>{this.state.phone}</p> 
+                                        <h3>Email</h3>
+                                        <p> {this.state.email}</p>   
+
+
+                                    </div>
+                                
+                                </Media>
                             </Media>
-                            <Media body className="">
-                                <div className="medBody">
-                                    <h3>Phone</h3>
-                                    <p>{this.state.phone}</p> 
-                                    <h3>Email</h3>
-                                    <p> {this.state.email}</p>   
-
-
-                                </div>
-                               
-                            </Media>
-                        </Media>
                              
 
                             {/* {this.state.profile_picture} */}
@@ -91,14 +94,18 @@ export default class FetchData extends React.Component {
                                 {this.state.album.map(single_album => (
                                     <Col xs="6" sm="4">
                                         <Card className="mb-3">
-                                            <div>
+                                            <div className="card-container">
                                              
                                               <CardImg top width="100%" src={require(`../${single_album.img}`) } alt="my photos" />
-                                              <h2>{single_album.title}</h2>
+                                              <h2 className="title">{single_album.title}</h2>
                                             </div>
                                             <CardBody>
-                                                <p>{single_album.description}</p>
-                                                 <p>{single_album.date}</p>
+                                                <div className="card-p">
+                                                 <p>{single_album.description}</p>
+                                                 <p className="p-right">{single_album.date}</p>
+
+                                                </div>
+                                               
                                             </CardBody>
 
 
