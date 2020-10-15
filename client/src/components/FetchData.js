3c1132ccfbf5fcc136505b91939c594f3b9b619c
Kyle Harrison <kyle.harrison344@gmail.com>
2020-10-15 18:36:52 -0400

    images loaded

--- client/src/components/FetchData.js
+++ client/src/components/FetchData.js
@@ -1,141 +1,9 @@
-// import React from 'react';
-// import { Container, Row, Col, Media } from 'reactstrap';
-// import '../App.css'
-// import 'bootstrap/dist/css/bootstrap.min.css'
-// import profilePic from '../components/profile.jpeg'
-
-
-// export default class FetchData extends React.Component {
-
-//     state = {
-//         loading: true,
-//         name: null,
-//         phone: null,
-//         email: null,
-//         bio: null,
-//         profile_picture: null,
-//         album: null
-        
-//     }
-   
-
-//     async componentDidMount() {
-//         const url = "http://localhost:3000/landscapes.json";
-//         const response = await fetch(url);
-//         const data = await response.json();
-//         this.setState({
-//             name: data.name, 
-//             phone: data.phone,
-//             email: data.email, 
-//             bio: data.bio, 
-//             profile_picture: data.profile_picture,
-//             album: [ ...data.album],
-           
-//             loading: false
-//             });
-//         // console.log( data.album[1]);
-//     }
-
-
-//     render() {
-//         console.log(this.state)
-//         // if (this.state.loading) {
-//         //     return <div>loading...</div>
-//         // } else
-//         return (
-//            <Container>
-//                <div>
-//                     <Row>
-//                         <Col> 
-//                         <Media className="my-3 p-3" >
-//                          <img className="Rounded proImg" src={profilePic} alt="profile picture" />
-//                             <Media heading className="p-3 medHead">
-//                                 <h1>
-//                                  {this.state.name}
-//                                 </h1>
-//                                 <p>
-//                                  {this.state.bio}
-
-//                                 </p>
-//                             </Media>
-//                             <Media body className="medBody">
-//                                 <h3>Phone</h3>
-//                                <p>{this.state.phone}</p> 
-//                                 <h3>Email</h3>
-//                                  <p> {this.state.email}</p>   
-
-//                             </Media>
-//                         </Media>
-                             
-
-//                             {/* {this.state.profile_picture} */}
-                           
-                      
-//                         </Col>
-//                     </Row>
-//                     <Row>
-//                         <Col xs="6" sm="4">{this.state.album.map}</Col>
-//                         <Col xs="6" sm="4">.col-6 .col-sm-4</Col>
-//                         <Col sm="4">.col-sm-4</Col>
-//                     </Row>
-//                     <Row>
-//                         <Col xs="6" sm="4">.col-6 .col-sm-4</Col>
-//                         <Col xs="6" sm="4">.col-6 .col-sm-4</Col>
-//                         <Col sm="4">.col-sm-4</Col>
-//                     </Row>
-
-//                </div>
-             
-         
-//            </Container>
-        
-            
-         
-//         )
-    
-//     }
-// }
-
-
-
-// import React from 'react'
-
-// export default class FetchData extends React.Component {
-
-//     state = {
-//         loading: true,
-      
-//     }
-
-
-//     async componentDidMount() {
-//         const url = "http://localhost:3000/landscapes.json";
-//         const response = await fetch(url);
-//         const data = await response.json();
-//         this.setState({person: data.results})
-//         console.log(data);
-//     }
-
-
-//     render() {
-//         return (
-//             <div>
-//             {this.state.loading || !this.state.person ? (
-//                 <div>loading...</div>
-//             )  : (
-//             <div>person..</div>)}
-//         </div>
-//         )
-    
-//     }
-// }
-
 import React, { Fragment } from 'react';
-import { Container, Row, Col, Media,Card, CardImg, CardText, CardBody,
+import { Container, Row, Col, Media,  Card, CardImg, CardText, CardBody,
     CardTitle } from 'reactstrap';
 import '../App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import profilePic from '../components/profile.jpeg'
+import profilePic from '../img/profile.jpg'
 
 
 export default class FetchData extends React.Component {
@@ -172,8 +40,9 @@ export default class FetchData extends React.Component {
     }
 
     
+    
     render() {
-        
+
         console.log(this.state);
 
         // if (this.state.loading) {
@@ -184,7 +53,7 @@ export default class FetchData extends React.Component {
                <div>
                     <Row>
                         <Col> 
-                        <Media className="my-3 p-3" >
+                        <Media className="my-3 p-3 mt-5" >
                          <img className="Rounded proImg" src={profilePic} alt="profile picture" />
                             <Media heading className="p-3 medHead">
                                 <h1>
@@ -195,12 +64,16 @@ export default class FetchData extends React.Component {
 
                                 </p>
                             </Media>
-                            <Media body className="medBody">
-                                <h3>Phone</h3>
-                               <p>{this.state.phone}</p> 
-                                <h3>Email</h3>
-                                 <p> {this.state.email}</p>   
+                            <Media body className="">
+                                <div className="medBody">
+                                    <h3>Phone</h3>
+                                    <p>{this.state.phone}</p> 
+                                    <h3>Email</h3>
+                                    <p> {this.state.email}</p>   
 
+
+                                </div>
+                               
                             </Media>
                         </Media>
                              
@@ -210,15 +83,17 @@ export default class FetchData extends React.Component {
                       
                         </Col>
                     </Row>
+                    {/* //cards */}
                     <Row>
                         {this.state.loading ? null
                         : 
                             <Fragment>
                                 {this.state.album.map(single_album => (
                                     <Col xs="6" sm="4">
-                                        <Card>
+                                        <Card className="mb-3">
                                             <div>
-                                              <CardImg top width="100%" src={this.state.single_album} alt="my photos" />
+                                             
+                                              <CardImg top width="100%" src={require(`../${single_album.img}`) } alt="my photos" />
                                               <h2>{single_album.title}</h2>
                                             </div>
                                             <CardBody>
@@ -233,15 +108,10 @@ export default class FetchData extends React.Component {
                                 ))}
                             </Fragment>
                         }
-                        {/* <Col xs="6" sm="4">{this.state.album.title}</Col> */}
-                        <Col xs="6" sm="4">.col-6 .col-sm-4</Col>
-                        <Col sm="4">.col-sm-4</Col>
+                       
+                       
                     </Row>
-                    <Row>
-                        <Col xs="6" sm="4">.col-6 .col-sm-4</Col>
-                        <Col xs="6" sm="4">.col-6 .col-sm-4</Col>
-                        <Col sm="4">.col-sm-4</Col>
-                    </Row>
+                 
 
                </div>
              
@@ -254,5 +124,3 @@ export default class FetchData extends React.Component {
     
     }
 }
-
-
